d22a0754731ea52dc3035ead311a5687fb2dfadb
mjosten <38707699+mjosten@users.noreply.github.com>
2019-01-25 20:39:46 -0800

    Add files via upload

--- src/DroidEntities.js
+++ src/DroidEntities.js
@@ -1,31 +1,148 @@
 /*
-Zerlin
+DroidEntites
 TCSS 491 - Computational Worlds
 Joshua Atherton, Michael Josten, Steven Golob
 */
+
+//Droid Constants
+var DROID_ZERLIN_MOVEMENT = 1; //amount a droid will move when left or right key is pressed
+//can potentially add in another zerlin move constant for rolling
+
+//basic droid constants
 var BASIC_DROID_SHOOT_INTERVAL = 120;
 var BASIC_DROID_X_MOVEMENT_SPEED = 150;
 var BASIC_DROID_Y_MOVEMENT_SPEED = 100;
-var BASIC_DROID_X_VELOCITY = 1;
+var BASIC_DROID_X_VELOCITY = 0.35;
 var BASIC_DROID_Y_VELOCITY = 1;
-var BASIC_DROID_ORBITAL_HEIGHT = 200;
-//Math is weird, higher the number, lower the speed. couldn't think of anyway to make this work
-//possible refinement needed. use a number between 0 and 1 for speed up;
+var BASIC_DROID_ORBITAL_X_OFFSET = 0;
+var BASIC_DROID_ORBITAL_Y_OFFSET = -200;
+var BASIC_DROID_ZERLIN_MOVE_RIGHT_SPEED = 1;
+
+//random constants ("interesting")
+var BASIC_DROID_MAX_RANDOM_TARGET_WIDTH = 40;
+var BASIC_DROID_MAX_RANDOM_TARGET_HEIGHT = 40;
+
+//Laser constants
 var BASIC_DROID_LASER_SPEED = 100; 
 var BASIC_DROID_LASER_LENGTH = 10;
-var BASIC_DROID_LASER_WIDTH = 10;
+var BASIC_DROID_LASER_WIDTH = 20;
+
+/**
+ * This class will serve as the parent for all droid entities
+ * and will contain methods and fields that all droids are required to have
+ * ***! Movement and Shooting Pattern will be implemented by child droids !***
+ */
+class AbstractDroid extends Entity {
+    constructor(game, startX, startY, deltaX, deltaY) {
+        super(game, startX, startY, deltaX, deltaY);
+        // if the animations are null, then will just not draw the animations.
+        this.idleAnimation = null; //need to add the animation after instantiation.
+        this.explosionAnimation = null;
+        this.shootAnimation = null;
+        this.animation = null;
+
+        //collision radius can be changed after instantiation
+        this.radius = 35;
+        this.boundCircle = {x: this.x, y: this.y, radius: this.radius};
+
+    }
+    /**
+     * this method will change the state of each droid such as firing 
+     * and moving and will be implemented more in each childDroid
+     */
+    update() {
+        // All droids will move when left or right is pressed but not both at the same time
+        if (this.game.keys['KeyD'] && this.game.keys['KeyA']);
+        else if (this.game.keys['KeyD']) {
+            this.x = this.x - DROID_ZERLIN_MOVEMENT;
+        } 
+        else if (this.game.keys['KeyA']) {
+            this.x = this.x + DROID_ZERLIN_MOVEMENT;
+        }
+
+        //check collision with other droids.
+        for (var i = 0; i < this.game.entities.length; i++) {
+            var ent = this.game.entities[i];
+            if (this != ent && ent instanceof AbstractDroid && this.collide(ent)) {
+                //On droid collision, swap velocities
+                var tempX = this.deltaX;
+                var tempY = this.deltaY;
+                this.deltaX = ent.deltaX;
+                this.deltaY = ent.deltaY;
+                ent.deltaX = tempX;
+                ent.deltaY = tempY;
+                //may need to shunt droids a little.
+            };
+        }
+
+        super.update();
+    }
+    draw() {
+        //debug: draw the bounding circle around the droid
+        if (this.game.showOutlines) {
+            this.game.ctx.beginPath();
+            this.game.ctx.strokeStyle = "green";
+            this.game.ctx.arc(this.boundCircle.x, 
+                this.boundCircle.y, this.boundCircle.radius, 0, Math.PI * 2, false);
+            this.game.ctx.stroke();
+            this.game.ctx.closePath();
+            this.game.ctx.closePath();
+            this.game.ctx.restore(); 
+        }
+        //child droid can choose which animation is the current one 
+        // check that animation is not null before drawing.
+        if (this.animation) {
+            this.animation.drawFrame(this.game.clockTick, this.game.ctx, this.x, this.y);
+        }
+        super.draw();
+    }
+    /**
+     * this method will remove the droid from the world and add an explosion to the entity list.
+     */
+    explode() {
+        this.removeFromWorld = true;
+        //TODO: play droid explosion sound
+        this.game.addEntity(new DroidExplosion(this.game, this.x, this.y));
+        console.log("droid exploded");
+    }
+    
+}
+
+
+
 /*
 * Basic droid that will shoot 1 laser every interval
 */
-class BasicDroid extends Entity {
+class BasicDroid extends AbstractDroid {
     constructor(game, spritesheet, startX, startY) {
         //super(gameEngine, x, y, deltaX, deltaY)
-        super(game, startX, startY, BASIC_DROID_X_MOVEMENT_SPEED, 0);
+        //super(game, startX, startY, BASIC_DROID_X_MOVEMENT_SPEED, 0);
+        super(game, startX, startY, 0, 0); //debug
+
+        /* animation fields */
         //Animation(spritesheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale)
-        this.animation = new Animation(spritesheet, 27, 33, 182, 0.5, 3, true, 1);
-        this.ctx = game.ctx;
+        this.idleAnimation = new Animation(spritesheet, 100, 100, 1400, 0.1, 14, true, 0.5);
+        this.animation = this.idleAnimation;
+        
+        /* animation frame width, height and scale for calculating bounding circle */
+        this.frameHeight = this.animation.frameHeight * this.animation.scale;
+        this.frameWidth = this.animation.frameWidth * this.animation.scale;
+        this.scale = this.animation.scale;
+
+        /* bounding circle fields */
+        this.radius = 25;
+        this.boundCircle = {radius: this.radius, 
+            x: this.x + this.frameWidth * this.scale,
+            y: this.y + this.frameHeight * this.scale};
+
+        /* shooting fields */
         this.fire = false;
         this.ticksBeforeFire = BASIC_DROID_SHOOT_INTERVAL
+
+        /* movement fields */
+        var targetX = (this.game.surfaceWidth / 2) + BASIC_DROID_ORBITAL_X_OFFSET;
+        var targetY = (this.game.surfaceHeight / 2) + BASIC_DROID_ORBITAL_Y_OFFSET;
+        this.targetOrbitalPoint = {x: targetX, y: targetY};
         
     }
     /* 
@@ -34,62 +151,84 @@ class BasicDroid extends Entity {
     * The droid will set removeFromWorld to true when it collides with lightsaber
     */
     update() {
-        //draw it so will circle around above zerlin
-        this.calcMovement(this.game.entities[0]);
+        //update coordinates so the droid will orbit the center of the canvas
+
+        /* droid movement */
+        //this.calcMovement(this.targetOrbitalPoint); //un comment after debug
+
+        /* bounding circle movement */
+        this.boundCircle.x = this.x + this.frameWidth * this.scale;
+        this.boundCircle.y = this.y + this.frameHeight * this.scale;
+
+        /* droid shooting */
         this.ticksBeforeFire--;
         //will shoot at every interval
         if (this.ticksBeforeFire <= 0 && (!this.fire)) {
             this.ticksBeforeFire = BASIC_DROID_SHOOT_INTERVAL;
             this.fire = true;
-            this.shoot(this.game.mouse.x, this.game.mouse.y);
-        }
-        if (this.game.keys['KeyD']) {
-            this.x = this.x - 1;
-        }
+            //shoot at specific target
+            //this.shoot(this.game.Zerlin.x, this.game.Zerlin.y);
 
+            //shoot randomly in target direction
+            // this.shootRandom(this.game.Zerlin.x + 50, 
+            //     this.game.Zerlin.y + 50, 
+            //     BASIC_DROID_MAX_RANDOM_TARGET_WIDTH,
+            //     BASIC_DROID_MAX_RANDOM_TARGET_HEIGHT);
+        }
+        
+        
         super.update();
     }
     draw() {
-        this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-        super.draw(this);
-        // Entity.prototype.draw.call(this);
+        super.draw();
+        
+        
     }
     shoot(targetX, targetY) {
         var droidLaser = new DroidLaser(this.game, this.x + 20, this.y + 20, BASIC_DROID_LASER_SPEED, 
             targetX, targetY, BASIC_DROID_LASER_LENGTH, BASIC_DROID_LASER_WIDTH);
         this.game.addEntity(droidLaser);
         //TODO - play shooting sound
-        console.log("shot laser at X: " + targetX + " Y: " + targetY);
+        //console.log("shot laser at X: " + targetX + " Y: " + targetY);
         //set after droid is done firing
         this.fire = false;
     }
+    /**
+     * Method that will shoot a laser randomly in an area from target point
+     * to target point + max argument
+     */
+    shootRandom(targetX, targetY, maxWidth, maxHeight) {
+        var randTargetX = targetX + (maxWidth * Math.random());
+        var randTargetY = targetY + ((-maxHeight) * Math.random());
+        this.shoot(randTargetX, randTargetY);
+    }
     /*
      * calculate movement so that it will try to fly around the location of the 
-     * entity.
+     * target.
      */
-    calcMovement(entity) {
-        //if the droid is to the left of zerlin, then increase the deltaX
+    calcMovement(target) {
+        //if the droid is to the left of target point, then increase the deltaX
         //by the x velocity
-        if (this.x < entity.x) {
+        if (this.x < target.x) {
             if (this.deltaX < BASIC_DROID_X_MOVEMENT_SPEED)
                 this.deltaX += BASIC_DROID_X_VELOCITY;
             
         }
         
-        //if the droid is to the right of zerlin, then decrease the deltaX
+        //if the droid is to the right of target point, then decrease the deltaX
         //by the x velocity
-        else if (this.x > entity.x) {
+        else if (this.x > target.x) {
             if (this.deltaX >= (-BASIC_DROID_X_MOVEMENT_SPEED))
                 this.deltaX -= BASIC_DROID_X_VELOCITY;
         }
 
-        //if droid is above the oribital height, then increase deltaY(down)
-        if (this.y < entity.y - BASIC_DROID_ORBITAL_HEIGHT) {
+        //if droid is above the target point, then increase deltaY(down)
+        if (this.y < target.y) {
             if (this.deltaY <= BASIC_DROID_Y_MOVEMENT_SPEED)
                 this.deltaY += BASIC_DROID_Y_VELOCITY;
         }
-        //if the droid is below the orbital height, then decrease the deltaY(up)
-        else if (this.y >= entity.y - BASIC_DROID_ORBITAL_HEIGHT) {
+        //if the droid is below the target point, then decrease the deltaY(up)
+        else if (this.y >= target.y) {
             if (this.deltaY >= (-BASIC_DROID_Y_MOVEMENT_SPEED)) 
                 this.deltaY -= BASIC_DROID_Y_VELOCITY;
         }      
@@ -104,119 +243,134 @@ class BasicDroid extends Entity {
     }
 }
 
+
+
+
 class DroidLaser extends Entity {
     constructor(game, startX, startY, speed, targetX, targetY, length, width) {
         super(game, startX, startY, 0, 0);
         //console.log("created DroidLaser Entity");
-        this.speed = speed;
-        this.targetX = targetX;
-        this.targetY = targetY;
+
+        //Droid Laser Fields
+        this.color = "green";
+        this.secondaryColor = "white";
+        this.angle = 0 //TODO: find angle of line with arctan???
+        this.isDeflected = false;
+
+        var distFromStartToTarget = distance({x: targetX, y: targetY}, {x: this.x, y: this.y});
+        var unitVectorDeltaX = ((targetX - startX) / distFromStartToTarget);
+        var unitVectorDeltaY = ((targetY - startY) / distFromStartToTarget);
+
+        this.deltaX = unitVectorDeltaX * speed;
+        this.deltaY = unitVectorDeltaY * speed;
+
         this.length = length;
         this.width = width;
 
-        this.deltaX = this.targetX - this.x;
-        this.deltaY = this.targetY - this.y;
-        this.slope = (this.deltaY * 1.0) / this.deltaX;
-        var newEndPoint = this.extendTargetPoint();
-        this.targetX = newEndPoint.x;
-        this.targetY = newEndPoint.y;
-        this.deltaX = this.targetX - this.x;
-        this.deltaY = this.targetY - this.y;
-        // console.log("dx = %f, dy = %f", this.deltaX, this.deltaY);
-        // console.log("slope = %f", this.slope);
-        // console.log("draw from X: %d, Y: %d, TO X: %d, Y: %d", this.x, this.y, this.targetX, this.targetY);
-
+        // move laser so tail is touching the starting point upon instantiation, instead of the head
+        this.x = this.x + unitVectorDeltaX * this.length;
+        this.y = this.y + unitVectorDeltaY * this.length;
+        this.tailX = startX;
+        this.tailY = startY;
     }
     update() {
-        //if laser goes out of bounds then remove from world
-        if (this.outsideScreen()) {
-            this.removeFromWorld = true;
-            //console.log("laser removed from world");
-        } else {
-            //change the x and y coordinates 
+        this.removeFromWorld = this.outsideScreen(); // will be removed in GameEngine
 
-            //Speed formula of lasers, can modify with speed field
-            var d = Math.sqrt(Math.pow((this.targetX - this.x), 2) + Math.pow((this.targetY - this.y), 2));
-            this.x += (this.deltaX / d) * this.game.clockTick * this.speed;
-            this.y += (this.deltaY / d) * this.game.clockTick * this.speed;
-            
+        this.x += this.deltaX * this.game.clockTick;
+        this.y += this.deltaY * this.game.clockTick;
+        this.tailX += this.deltaX * this.game.clockTick;
+        this.tailY += this.deltaY * this.game.clockTick;
+
+        //check collision with droid
+        for (var i = 0; i < this.game.entities.length; i++) {
+            var ent = this.game.entities[i];
+            if (this != ent && ent instanceof AbstractDroid && this.collideWithDroid(ent)) {
+                console.log("Collision with droid");
+                this.color = "red";
+                //explode the droid.
+                ent.explode();
+                //increment points
+            }
         }
+    
         super.update();
     }
     draw() {
         var ctx = this.game.ctx;
-        //draw a line of size length along a tragectory between start and end points.
-        var nextPoint = this.calcNextPoint(this.targetX, this.targetY);
-        
+
+        //debug laser
+        // ctx.save();
+        // ctx.lineWidth = 10;
+        // ctx.strokeStyle = this.color;
+        // ctx.lineCap = "round";
+        // ctx.beginPath();
+        // ctx.moveTo(this.x, this.y);
+        // ctx.lineTo(this.tailX, this.tailY);
+        // ctx.stroke();
+        // ctx.closePath();
+        // ctx.restore();
+        //end debug code
+
         ctx.save();
-        //green outer layer of laser
+        //Outer Layer of laser
         ctx.lineWidth = this.width;
-        ctx.strokeStyle = "green";
+        ctx.strokeStyle = this.color;
         ctx.lineCap = "round";
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
-        ctx.lineTo(nextPoint.x, nextPoint.y);
+        ctx.lineTo(this.tailX, this.tailY);
         ctx.stroke();
         ctx.closePath();
 
-        //white inner layer of laser.
+        //inner layer of laser.
         ctx.lineWidth = this.width / 2;
-        ctx.strokeStyle = "white";
+        ctx.strokeStyle = this.secondaryColor;
         ctx.lineCap = "round";
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
-        ctx.lineTo(nextPoint.x, nextPoint.y);
+        ctx.lineTo(this.tailX, this.tailY);
         ctx.stroke();
         ctx.closePath();
-
         ctx.restore();
 
         super.draw()
     }
-    /*
-    * this function will extend the target parameters to go past the 
-    * borders of the canvas.
-    * This is mainly to solve a bug in the calcNextPoint method
-    * where when the start point overlappes the end point then it shifts and draws backwards.
-    */
-    extendTargetPoint() {
-        // need to find a point that is accross the canvas boundries 
-        // that is also in a line.
-        var tempX = this.targetX;
-        var tempY = this.targetY;
-        //keep suming tempX and tempY by deltaX and deltaY until the temp X or Y
-        // is less than 0 or is greater than max width or height.
-        while (tempX > -10 && tempX < this.game.surfaceWidth + 10 ||
-            tempY > -10 && tempY < this.game.surfaceHeight + 10) {
 
-            tempX += this.deltaX;
-            tempY += this.deltaY;
+    /**
+     * This method will return a boolean if there is a collision between the laser segment and
+     * the droid.
+     * @param {AbstractDroid} otherDroid the droid to check collision with
+     */
+    collideWithDroid(otherDroid) {
+        return collideLineWithCircle(this.x, this.y, this.tailX, this.tailY, otherDroid.boundCircle.x,
+            otherDroid.boundCircle.y, otherDroid.boundCircle.radius);
+        
+    }
+
+}
+
+/**
+ * this class will just play the droid explosion animation
+ * and when the animation is done, this entity will be removed from world
+ */
+class DroidExplosion extends Entity {
+    constructor(game, x, y) {
+        super(game, x, y, 0, 0);
+        
+        var spritesheet = this.game.assetManager.getAsset("../img/Explosion.png");
+        //Animation(spritesheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale)
+        this.animation = new Animation(spritesheet, 64, 64, 256, 0.2, 15, false, 0.8);
+    }
+    update() {
+        super.update();
+        if (this.animation.isDone()) {
+            this.removeFromWorld = true;
         }
-        return {x: tempX, y: tempY};
-        
     }
-    /*
-    * This function will calculate the next point along the path of the 
-    * laser tragectory allowing a line to be drawn from the incrementing 
-    * x, y to an arbitrary point some length away.
-    */
-    calcNextPoint(x1, y1) {
-
-        //d is the distance between (x,y) and the target x and y
-        var d = Math.sqrt(Math.pow((x1 - this.x), 2) + Math.pow((y1 - this.y), 2));
-        //t is the ratio of distances
-        var t = this.length / d;
-        var xt = (1 - t) * this.x + (t * x1);
-        var yt = (1 - t) * this.y + (t * y1);
-        // console.log("START X: %f, Y: %f", this.x, this.y);
-        // console.log("NEXT X: %f, Y: %f", xt, yt);
-        // console.log("TARGET X: %f, Y: %f", x1, y1);
-        
-        return {x: xt, y: yt};
-
-
+    draw() {
+        this.animation.drawFrame(this.game.clockTick, this.game.ctx, this.x, this.y);
+        super.draw(this.game.ctx);
     }
-
 }
 
 
